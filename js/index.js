5679aa0e7f31adf1b880652d6515737b7653b683
bihuibin <bihuibin@163.com>
2019-01-08 17:36:49 +0800

    完成开机动画，项目完成

--- js/index.js
+++ js/index.js
@@ -15,16 +15,120 @@
     //获取第五屏ul
     var teamListsNodes = document.querySelectorAll('.team-inner .team-lists li');
     var teamListsNode = document.querySelector('.team-inner .team-lists');
-
-
     //获取contentNode高度
     var contentH = contentNode.offsetHeight;
+    //获取第一屏
+      var homeNode = document.querySelector('.homme .homme-inner');
+    //  第二屏元素
+      var planeT = document.querySelector('.plane-top');
+      var planeC = document.querySelector('.plane-center');
+      var planeB = document.querySelector('.plane-bottom');
+      var plane1 = document.querySelector('.pencel1');
+      var plane2 = document.querySelector('.pencel2');
+      var plane3 = document.querySelector('.pencel3');
+      var aboutPsT = document.querySelector('.about-inner .about-shrink-top');
+      var aboutPsB = document.querySelector('.about-inner .about-shrink-bottom');
     //定义时间接受变量
     var timer= 0;
     //定义一个计数器
     var num = 0;
     var nowIndex = 0;
     var lastIndex = 0;
+    
+        //出入场动画
+    var animationArr = [
+      {
+        anout:function () {
+          homeNode.style.transform = 'translateY(-200px)';
+          homeNode.style.opacity= '0'
+        },
+        anint:function () {
+          homeNode.style.transform = 'translateY( 0 )';
+          homeNode.style.opacity= '1'
+        }
+      },
+      {
+        anout:function () {
+          planeT.style.transform = 'translate(-200px , -200px)';
+          planeC.style.transform = 'translate(-200px , 200px)';
+          planeB.style.transform = 'translate(200px , -200px)';
+        },
+        anint:function () {
+          planeT.style.transform = 'translate(0 , 0)';
+          planeC.style.transform = 'translate(0 , 0)';
+          planeB.style.transform = 'translate(0 , 0)';
+        }
+      },
+      {
+        anout:function () {
+          plane1.style.transform = 'translate(-200px , 0)';
+          plane2.style.transform = 'translate(-200px , 200px)';
+          plane3.style.transform = 'translate(200px , 200px)';
+        },
+        anint:function () {
+          plane1.style.transform = 'translate(0 , 0)';
+          plane2.style.transform = 'translate(0 , 0)';
+          plane3.style.transform = 'translate(0 , 0)';
+        }
+      },
+      {
+        anout:function () {
+          aboutPsT.style.transform = 'rotate(30deg)';
+          aboutPsB.style.transform = 'rotate(-30deg)';
+        },
+        anint:function () {
+          aboutPsT.style.transform = 'rotate(0)';
+          aboutPsB.style.transform = 'rotate(0)';
+        }
+      },
+      {
+        anout: function () {
+          // aboutPsT.style.transform = 'rotate(30deg)';
+          // aboutPsB.style.transform = 'rotate(-30deg)';
+        },
+        anint: function () {}
+      }
+    ]
+        // 出入场动画
+      for (var i = 0; i < animationArr.length; i++) {
+          animationArr[i].anout();
+      }
+
+        //开机动画
+      bootAnimation();
+    function bootAnimation() {
+      var bootAnimationTopNode = document.querySelector('.boot-animation .top');
+      var bootAnimationBottomNode = document.querySelector('.boot-animation .bottom');
+      var bootAnimationLineNode = document.querySelector('.boot-animation .line');
+      var bootAnimationNode = document.querySelector('.boot-animation');
+      var imgArr = ['bg1.jpg','bg2.jpg','bg3.jpg','bg4.jpg','bg5.jpg','about1.jpg','about2.jpg','about3.jpg',
+        'about4.jpg','worksimg1.jpg','worksimg2.jpg','worksimg3.jpg','worksimg4.jpg','team.png','greenLine.png'];
+      var length = imgArr.length;
+      var num = 0;
+      for (var i = 0; i < length; i++) {
+
+        var item = imgArr[i];
+        var image = new Image();
+        image.src = './img/'+ item;
+
+        image.onload = function () {
+          num++;
+          bootAnimationLineNode.style.width = num / length * 100 +'%';
+          console.log(bootAnimationLineNode.style.width);
+          if(num === length){
+            console.log(num)
+            console.log(length)
+            bootAnimationTopNode.style.height = 0;
+            bootAnimationBottomNode.style.height = 0;
+            bootAnimationLineNode.style.display = 'none';
+          }
+          bootAnimationNode.addEventListener('transitionend',function () {
+            bootAnimationNode.remove();
+            animationArr[0].anint();
+          })
+        }
+      }
+    }
         //头部部分
     headerHandle();
     function headerHandle() {
@@ -34,7 +138,7 @@
             //设置down的默认宽度100%
             downNodes[0].style.width = '100%';
             //设置小箭头初始位置
-            arrowNode.style.left = headerInnerlis[0].getBoundingClientRect().left + headerInnerlis[0].offsetWidth/2 - arrowWidthNode + 'px';
+            arrowNode.style.left = headerInnerlis[nowIndex].getBoundingClientRect().left + headerInnerlis[nowIndex].offsetWidth/2 - arrowWidthNode + 'px';
         }
        //
 
@@ -42,41 +146,40 @@
         for (var i = 0; i < headerInnerlis.length; i++) {
             //给所有li添加一个index属性
             headerInnerlis[i].index = i;
-            contentRightDotNodes[i].index = i;
+            // contentRightDotNodes[i].index = i;
             headerInnerlis[i].onclick = function () {
                 //清除上次的样式
-                contentRightDotNodes[lastIndex].className = '';
-                downNodes[lastIndex].style.width = '';
+                // contentRightDotNodes[lastIndex].className = '';
+                // downNodes[lastIndex].style.width = '';
                 nowIndex = this.index;
                 move(nowIndex)
                 // console.log(num)
                 // console.log(arrowWidthNode)
             }
-            //内容区右侧小圆点移动
-            contentRightDotNodes[i].onclick = function () {
-                //清除上次的样式
-              contentRightDotNodes[lastIndex].className = '';
-                nowIndex = this.index;
-                lastIndex = nowIndex;
-                move(nowIndex)
-            }
+
         }
     }
 
+
+
     // 公共区域move
     function move(nowIndex) {
-      contentRightDotNodes[lastIndex].className = '';
+
       //清除上次的样式
       downNodes[lastIndex].style.width = '';
+      contentRightDotNodes[lastIndex].className = '';
         //设置当前小箭头宽度为100%
         downNodes[nowIndex].style.width = '100%';
         // console.log(this.index)
         //小箭头移动
+
         arrowNode.style.left = headerInnerlis[nowIndex].getBoundingClientRect().left +headerInnerlis[nowIndex].offsetWidth/2 - arrowWidthNode +'px';
         //内容区移动
         contentInnerNode.style.top = nowIndex * -contentH + 'px';
         //内容区右侧小圆点移动
       contentRightDotNodes[nowIndex].className = 'active';
+      animationArr[lastIndex].anout();
+      animationArr[nowIndex].anint();
         lastIndex = nowIndex;
     }
 
@@ -87,6 +190,7 @@
            document.onmousewheel = wheel;
            document.addEventListener('DOMMousescroll',wheel);
            //创建wheel函数
+
            function wheel(event) {
                event = event||window.event;
                    //清除上次定时器
@@ -116,25 +220,26 @@
                        //判断flag
                        switch (flag){
                            case 'up':
-                               if(num > 0){
-                                   num--;
-                                   move(num);
-                                   console.log(num)
+                               if(nowIndex > 0){
+                                 nowIndex--;
+                                   move(nowIndex);
+                                   // console.log(num)
                                }
                                break;
                            case 'down':
-                               if(num < headerInnerlis.length-1){
-                                   num++;
-                                   move(num);
+                               if(nowIndex < headerInnerlis.length-1){
+                                 nowIndex++;
+                                   move(nowIndex);
                                    // console.log(num)
                                }
                                break;
                        }
                    },150);
-
+             // lastIndex = nowIndex;
                //禁止默认行为
                event.preventDefault && event.preventDefault();
                return false;
+
            }
     }
 
@@ -142,13 +247,25 @@
     window.onresize = function () {
         //解决打开控制台小箭头位置
 
-        arrowNode.style.left = headerInnerlis[num].getBoundingClientRect().left + headerInnerlis[num].offsetWidth/2 - arrowWidthNode+'px';
+        arrowNode.style.left = headerInnerlis[nowIndex].getBoundingClientRect().left + headerInnerlis[nowIndex].offsetWidth/2 - arrowWidthNode+'px';
         //解决打开控制台内容区位置
         // console.log(arrowNode.offsetLeft)
-        contentInnerNode.style.top = num * -contentH+'px';
+        contentInnerNode.style.top = nowIndex * -contentH+'px';
 
     }
 
+      for (var i = 0; i < contentRightDotNodes.length; i++) {
+        //内容区右侧小圆点移动
+        // contentRightDotNodes[lastIndex].className = '';
+        contentRightDotNodes[i].index = i;
+        contentRightDotNodes[i].onclick = function () {
+          // //清除上次的样式
+
+          nowIndex = this.index;
+          move(nowIndex)
+        }
+
+      }
 
     //  第一屏部分
     firstScreenHandle();
@@ -299,8 +416,8 @@
                 s : s,
                 c_r : c_r,
                 deg : 0
-              },50)
-            })
+              })
+            },50)
               //创建圆
              timer2 = setInterval(function () {
                //清除画布山上次的元素
@@ -324,7 +441,7 @@
                   //开始画
                   ctx.beginPath();
                   //画圆
-                  ctx.arc(x,y,itms.c_r,0,2 * Math.PI);
+                  ctx.arc(x , y , itms. c_r, 0 , 2 * Math.PI);
                   //填充圆
                   ctx.fill();
                 }
